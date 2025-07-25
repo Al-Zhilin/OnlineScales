a6064f9fb0119da710ec25eff289d294f3f224e2
Alexey Zhilin <alekseyzh5@gmail.com>
2025-07-25 20:55:51 +0300

    some job

--- OnlineScales.ino
+++ OnlineScales.ino
@@ -1,6 +1,8 @@
 #include <GyverDS18.h>
 #include <GyverWDT.h>
 #include <GyverHX711.h>
+#include <EncButton.h>
+#include "passwords.h"
 
 // ---------------------------------------------- AT-команды ----------------------------------------------
 #define CHECK_MODEM "AT"
@@ -20,16 +22,34 @@
 #define SCL_PIN 8           // SCK пин HX711
 // --------------------------------------------- Пины Arduino ---------------------------------------------
 
-GyverHX711 sensor(DT_PIN, SCK_PIN, HX_GAIN128_A);
+GyverHX711 sensor(DT_PIN, SCL_PIN, HX_GAIN64_A);
+Button tare_button(5);
 
 void setup() {
   Serial.begin(9600);
   sensor.tare();
   sensor.sleepMode(false);
+  tare_button.setBtnLevel(1);
 }
 
 void loop() {
   if (sensor.available()) {
-    Serial.println(sensor.read());
+    Serial.println(expRunningAverageAdaptive(sensor.read()));
   }
+
+  if (Serial.available()) {
+    if (Serial.parseInt() == 1)  sensor.tare();
+  }
+}
+
+int32_t expRunningAverageAdaptive(int32_t newVal) {
+  //return newVal;
+  static int32_t filVal = 0;
+  float k;
+  // резкость фильтра зависит от модуля разности значений
+  if (abs(newVal - filVal) > 50) k = 0.9;
+  else k = 0.05;
+
+  filVal += (newVal - filVal) * k;
+  return filVal;
 }
